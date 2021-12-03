dddeff3fbabc94f898e8609301ba2903bfed6d46
Danil Ochagov <danilochagov@gmail.com>
2021-12-03 13:07:24 +0300

    Keep parsing URLs after errors

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ for i in range(number_of_urls):
 
 
 def work(url):
-    """ Requesting JSON data (free seats) """
-    print("Запускаем мониторинг: " + url) 
+    """ Parsing free seats """
+    print(f"Запускаем мониторинг: {url}") 
 
     current_number_of_seats = 0
 
@@ -38,21 +38,15 @@ def work(url):
             
             if number_of_seats > current_number_of_seats:
                 current_number_of_seats = number_of_seats
-                print("Новые билеты по " + url)
+                print(f"Новые билеты по {url}")
 
                 for i in range(3):
                     playsound("success.mp3")
                     time.sleep(1)
-
-            time.sleep(interval)
         except Exception:
-            print("\n\n!!!ВОЗНИКЛА НЕИЗВЕСТНАЯ ОШИБКА!!!")
-
-            for i in range(5):
-                playsound("error.mp3")
-                time.sleep(1)
-
-            exit()
+            print(f"ВОЗНИКЛА НЕИЗВЕСТНАЯ ОШИБКА ПО URL = {url}")
+        finally:
+            time.sleep(interval)
 
 
 # Spawning our threads
