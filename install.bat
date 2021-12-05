2c63912a0b74597fbff538ab91c00d9fb93b67b2
Danil Ochagov <danilochagov@gmail.com>
2021-12-05 19:38:04 +0300

    Fix playsound error

--- install.bat
+++ install.bat
@@ -1,2 +1,2 @@
 :: run this file to set up the environment installation
-START cmd.exe /k "cd %CD% & py -m venv env & env\Scripts\activate & pip3 install requests & pip3 install bs4 & pip3 install playsound & deactivate & exit"
+START cmd.exe /k "cd %CD% & py -m venv env & env\Scripts\activate & pip3 install requests & pip3 install bs4 & pip3 install playsound==1.2.2 & deactivate & exit"
